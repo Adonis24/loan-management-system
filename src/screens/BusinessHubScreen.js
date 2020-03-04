0678754c31a49859866121ed7aafaa97f252799a
Hafizh <eva_01@yahoo.com>
2020-03-04 16:41:29 +0800

    tukar kpd format code baru di BusinessHubScreen

--- src/screens/BusinessHubScreen.js
+++ src/screens/BusinessHubScreen.js
@@ -28,98 +28,83 @@ import styles from '../styles/styles'
 import { connect } from 'react-redux'
 import * as actionCreator from '../store/actions/action'
 
-class BusinessHubScreen extends React.PureComponent {
-    static navigationOptions = {
-        header: null,
-    };
+const BusinessHubScreen = (props) => {
 
 
 
-    render() {
-        return (
-            <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
-                <View style={{ flex: 1, justifyContent: 'space-between' }}>
-                    <View style={{ alignItems: 'flex-end' }}>
-                        <Image source={require('../assets/images/topRight.png')} style={{ width: 140, height: 130 }} resizeMode={'contain'} />
-                    </View>
-                    <View style={{ alignItems: 'flex-start' }}>
-                        <Image source={require('../assets/images/bottomLeft.png')} style={{ width: 79, height: 143 }} resizeMode={'contain'} />
+    return (
+        <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
+            <View style={{ flex: 1, justifyContent: 'space-between' }}>
+                <View style={{ alignItems: 'flex-end' }}>
+                    <Image source={require('../assets/images/topRight.png')} style={{ width: 140, height: 130 }} resizeMode={'contain'} />
+                </View>
+                <View style={{ alignItems: 'flex-start' }}>
+                    <Image source={require('../assets/images/bottomLeft.png')} style={{ width: 79, height: 143 }} resizeMode={'contain'} />
+                </View>
+            </View>
+            <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, bottom: 0, right: 0, }}>
+                {/* HEADER */}
+                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <View style={{ flex: 1, marginLeft: 10, justifyContent: 'center', border: 1, borderColor: '#000' }}>
+                        <TouchableOpacity onPress={() => this.props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
+                            <Ionicons name='ios-arrow-back' size={32} />
+                        </TouchableOpacity>
                     </View>
                 </View>
-                <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, bottom: 0, right: 0, }}>
-                    {/* HEADER */}
-                    <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-                        <View style={{ flex: 1, marginLeft: 10, justifyContent: 'center', border: 1, borderColor: '#000' }}>
-                           <TouchableOpacity onPress={() => this.props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
-                                <Ionicons name='ios-arrow-back' size={32} />
-                            </TouchableOpacity>
+                {/* CONTENT AREA */}
+                <View style={{ flex: 7, justifyContent: 'flex-start' }}>
+                    <Text style={[styles.textDefault, { fontWeight: 'bold', alignSelf: 'center', marginBottom: 20, fontSize: 20 }]}>Business Hub</Text>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20, marginLeft: Layout.window.width / 10, marginRight: Layout.window.width / 10 }}>
+                        <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
+                            <Image source={require('../assets/images/EDA.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
+                            <Text style={[styles.caption]}>EDA</Text>
+                        </View>
+                        <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
+                            <Image source={require('../assets/images/directory.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
+                            <Text style={[styles.caption]}>Biz Directory</Text>
+                        </View>
+                        <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
+                            <Image source={require('../assets/images/CRM.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
+                            <Text style={[styles.caption]}>CRM</Text>
                         </View>
                     </View>
-                    {/* CONTENT AREA */}
-                    <View style={{ flex: 7, justifyContent:'flex-start' }}>
-                        <Text style={[styles.textDefault, { fontWeight: 'bold', alignSelf: 'center', marginBottom: 20, fontSize: 20 }]}>Business Hub</Text>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20,marginLeft:Layout.window.width/10,marginRight:Layout.window.width/10 }}>
-                            <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
-                                <Image source={require('../assets/images/EDA.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                                <Text style={[styles.caption]}>EDA</Text>
-                            </View>
-                            <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
-                                <Image source={require('../assets/images/directory.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                                <Text style={[styles.caption]}>Biz Directory</Text>
-                            </View>
-                            <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
-                                <Image source={require('../assets/images/CRM.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                                <Text style={[styles.caption]}>CRM</Text>
-                            </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20, marginLeft: Layout.window.width / 10, marginRight: Layout.window.width / 10 }}>
+                        <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
+                            <Image source={require('../assets/images/mayamall.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
+                            <Text style={[styles.caption]}>Marketplace</Text>
                         </View>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 20,marginLeft:Layout.window.width/10,marginRight:Layout.window.width/10 }}>
-                            <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
-                                <Image source={require('../assets/images/mayamall.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                                <Text style={[styles.caption]}>Marketplace</Text>
-                            </View>
-                            <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
-                                <Image source={require('../assets/images/e-scoring.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                                <Text style={[styles.caption]}>Financing</Text>
-                            </View>
-                            <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
-                                <Image source={require('../assets/images/grant.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                                <Text style={[styles.caption]}>Billing</Text>
-                            </View>
+                        <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
+                            <Image source={require('../assets/images/e-scoring.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
+                            <Text style={[styles.caption]}>Financing</Text>
                         </View>
-                        <View style={{ flexDirection: 'row', justifyContent: 'space-between',marginLeft:Layout.window.width/10,marginRight:Layout.window.width/10 }}>
-                            <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
-                                <Image source={require('../assets/images/ecommerce.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                                <Text style={[styles.caption]}>Dev hub</Text>
-                            </View>
-                            <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
-                                <Image source={require('../assets/images/RFQ.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                                <Text style={[styles.caption]}>RFQ</Text>
-                            </View>
-                            <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
-                                <Image source={require('../assets/images/e-scoring.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                                <Text style={[styles.caption]}>e-Scoring</Text>
-                            </View>
+                        <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
+                            <Image source={require('../assets/images/grant.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
+                            <Text style={[styles.caption]}>Billing</Text>
+                        </View>
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginLeft: Layout.window.width / 10, marginRight: Layout.window.width / 10 }}>
+                        <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
+                            <Image source={require('../assets/images/ecommerce.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
+                            <Text style={[styles.caption]}>Dev hub</Text>
+                        </View>
+                        <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
+                            <Image source={require('../assets/images/RFQ.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
+                            <Text style={[styles.caption]}>RFQ</Text>
+                        </View>
+                        <View style={[styles.shadow, { width: Layout.window.height / 10, height: Layout.window.height / 9.5, backgroundColor: '#fff', alignSelf: 'stretch', borderRadius: 10, borderWidth: 1, borderColor: '#ddd' }]}>
+                            <Image source={require('../assets/images/e-scoring.png')} style={{ width: undefined, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
+                            <Text style={[styles.caption]}>e-Scoring</Text>
                         </View>
                     </View>
                 </View>
             </View>
+        </View>
 
-        );
-    }
+    );
 }
 
 
-function mapStateToProps(state) {
-    return {
 
 
 
-    }
-}
-function mapDispatchToProps(dispatch) {
-    return {
-
-    }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(BusinessHubScreen)
+export default BusinessHubScreen
