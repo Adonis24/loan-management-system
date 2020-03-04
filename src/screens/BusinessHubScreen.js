57faa11ec7d762f6b11f57fff7b61edf34fa14ae
Hafizh <eva_01@yahoo.com>
2020-03-04 16:51:17 +0800

    tukar kepada format code baru at BusinessHubScreen

--- src/screens/BusinessHubScreen.js
+++ src/screens/BusinessHubScreen.js
@@ -46,7 +46,7 @@ const BusinessHubScreen = (props) => {
                 {/* HEADER */}
                 <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
                     <View style={{ flex: 1, marginLeft: 10, justifyContent: 'center', border: 1, borderColor: '#000' }}>
-                        <TouchableOpacity onPress={() => this.props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
+                        <TouchableOpacity onPress={() => props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
                             <Ionicons name='ios-arrow-back' size={32} />
                         </TouchableOpacity>
                     </View>
