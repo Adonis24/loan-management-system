361278d6c642bf7b108a83065bc7196261aa8cd6
Hafizh <eva_01@yahoo.com>
2020-03-05 12:05:37 +0800

    tukar kepada format code baru at Edonation

--- src/screens/EdonationScreen.js
+++ src/screens/EdonationScreen.js
@@ -1,5 +1,5 @@
 //console.ignoredYellowBox = ['Setting a timer']
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     Image,
     Platform,
@@ -18,7 +18,7 @@ import {
 
 import Constants from 'expo-constants'
 //import { Constants, LinearGradient, FileSystem } from 'expo'
-
+import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 import Layout from '../constants/Layout'
 
 import { Ionicons } from '@expo/vector-icons';
@@ -29,40 +29,33 @@ import { connect } from 'react-redux'
 import * as actionCreator from '../store/actions/action'
 import WebView from 'react-native-webview';
 
-class EdonationScreen extends React.PureComponent {
-    static navigationOptions = {
-        header: null,
-    };
 
-    componentDidMount() {
-        this.props.initiateDashboardScreen()
-        //this.props.navigation.navigate('PopupScore')
-    }
-    render() {
-        return (
-            <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
-                <WebView source={{ uri: 'https://www.ensani.my' }} style={{ flex: 1 }} />
-                <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <View style={{ flex: 1, marginLeft: 10, justifyContent: 'center', border: 1, borderColor: '#000' }}>
-                       <TouchableOpacity onPress={() => this.props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
-                            <Ionicons name='ios-arrow-back' size={32} color={'#fff'} />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ flex: 4, marginTop: 5, marginBottom: 5, paddingTop: 5, paddingBottom: 5 }}>
-                        <View style={[{ backgroundColor: 'transparent', marginLeft: Layout.window.width / 7, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', padding: 5, height: Layout.window.height / 15 }]} />
-                    </View>
+const EdonationScreen = (props) => {
+
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(actionCreator.initiateDashboardScreen())
+    }, []); // empty-array means don't watch for any updates
+
+    return (
+        <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
+            <WebView source={{ uri: 'https://www.ensani.my' }} style={{ flex: 1 }} />
+            <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+                <View style={{ flex: 1, marginLeft: 10, justifyContent: 'center', border: 1, borderColor: '#000' }}>
+                    <TouchableOpacity onPress={() => props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
+                        <Ionicons name='ios-arrow-back' size={32} color={'#fff'} />
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flex: 4, marginTop: 5, marginBottom: 5, paddingTop: 5, paddingBottom: 5 }}>
+                    <View style={[{ backgroundColor: 'transparent', marginLeft: Layout.window.width / 7, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', padding: 5, height: Layout.window.height / 15 }]} />
                 </View>
             </View>
-        );
-    }
+        </View>
+    );
+
 }
 
-function mapStateToProps(state) {
-    return {}
-}
-function mapDispatchToProps(dispatch) {
-    return {
-        initiateDashboardScreen: () => dispatch(actionCreator.initiateDashboardScreen())
-    }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(EdonationScreen)
+
+
+export default EdonationScreen
