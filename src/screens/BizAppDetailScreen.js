17e2268e962f482c03be84e0a160f51eaeb10c35
Hafizh <eva_01@yahoo.com>
2020-03-05 12:03:35 +0800

    tukar kepada format code baru at BizAppDetail

--- src/screens/BizAppDetailScreen.js
+++ src/screens/BizAppDetailScreen.js
@@ -28,42 +28,30 @@ import { Tabs, Tab, ScrollableTab, Drawer, Container, Header, Content, Footer, L
 import { connect } from 'react-redux'
 import * as actionCreator from '../store/actions/action'
 
-class BizAppDetailScreen extends React.PureComponent {
-    static navigationOptions = {
-        header: null,
-    };
 
-    render() {
-        const uri = this.props.navigation.getParam('uri', 'NA')
-        return (
-            <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
-                <WebView style={{ flex: 1 }} source={{ uri }} />
-                <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <View style={{ flex: 1, marginLeft: 10, justifyContent: 'center', border: 1, borderColor: '#000' }}>
-                        <TouchableOpacity onPress={() => this.props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
-                            <Ionicons name='ios-arrow-back' size={32} color={'#fff'} />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ flex: 4, marginTop: 5, marginBottom: 5, paddingTop: 5, paddingBottom: 5 }}>
-                        <View style={[{ backgroundColor: 'transparent', marginLeft: Layout.window.width / 7, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', padding: 5, height: Layout.window.height / 15 }]} />
-                    </View>
+
+const BizAppDetailScreen = (props) => {
+
+    const uri = props.route.param?.uri ?? 'NA' 
+    
+    return (
+        <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
+            <WebView style={{ flex: 1 }} source={{ uri }} />
+            <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
+                <View style={{ flex: 1, marginLeft: 10, justifyContent: 'center', border: 1, borderColor: '#000' }}>
+                    <TouchableOpacity onPress={() => this.props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
+                        <Ionicons name='ios-arrow-back' size={32} color={'#fff'} />
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flex: 4, marginTop: 5, marginBottom: 5, paddingTop: 5, paddingBottom: 5 }}>
+                    <View style={[{ backgroundColor: 'transparent', marginLeft: Layout.window.width / 7, flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center', padding: 5, height: Layout.window.height / 15 }]} />
                 </View>
             </View>
-        );
-    }
+        </View>
+    );
+
 }
 
 
-function mapStateToProps(state) {
-    return {
 
-
-
-    }
-}
-function mapDispatchToProps(dispatch) {
-    return {
-
-    }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(BizAppDetailScreen)
+export default BizAppDetailScreen
