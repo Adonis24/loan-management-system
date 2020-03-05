9f34155d68707dc276804656260e6a30fc5cb5c7
Hafizh <eva_01@yahoo.com>
2020-03-05 12:06:59 +0800

    tukar kepada format code baru at Elearning

--- src/screens/ElearningScreen.js
+++ src/screens/ElearningScreen.js
@@ -19,7 +19,7 @@ import {
 
 import Constants from 'expo-constants'
 import { LinearGradient } from 'expo-linear-gradient'
-
+import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 import Layout from '../constants/Layout'
 
 import { Ionicons } from '@expo/vector-icons';
@@ -30,34 +30,33 @@ import { connect } from 'react-redux'
 import * as actionCreator from '../store/actions/action'
 
 
-class ElearningScreen extends React.PureComponent {
-    static navigationOptions = {
-        header: null,
-    };
-    nav = (screen) => {
+
+const ElearningScreen = (props) => {
+
+    const nav = (screen) => {
         this.props.navigation.navigate(screen)
     }
-    render() {
-        const { jwt } = this.props
-        console.log(`jwt ialah ${jwt}`)
-        const uri = `https://lms.bxcess.my/courses/?bx-token=${jwt}`
 
-        return (
-            <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
-                <WebView source={{ uri }} style={{ flex: 1,backgroundColor:'transparent' }} />
-              
-            <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0,right:0, flex: 1, flexDirection: 'row', justifyContent: 'center',alignItems:'center' }}>
-                <View style={{ flex: 1,alignSelf:'stretch', justifyContent: 'center', border: 1, borderColor: '#000',alignItems:'center' }}>
+    const { jwt } = useSelector(state => state.myAccountReducer, shallowEqual)
+    console.log(`jwt ialah ${jwt}`)
+    const uri = `https://lms.bxcess.my/courses/?bx-token=${jwt}`
+
+    return (
+        <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
+            <WebView source={{ uri }} style={{ flex: 1, backgroundColor: 'transparent' }} />
+
+            <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, right: 0, flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                <View style={{ flex: 1, alignSelf: 'stretch', justifyContent: 'center', border: 1, borderColor: '#000', alignItems: 'center' }}>
                     <TouchableOpacity onPress={() => this.props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
-                       <View style={{borderWidth:3,borderColor:'#fff',width:Layout.window.width/5}} />
+                        <View style={{ borderWidth: 3, borderColor: '#fff', width: Layout.window.width / 5 }} />
                     </TouchableOpacity>
                 </View>
-              
-            </View>
-            </View>
 
-        );
-    }
+            </View>
+        </View>
+
+    );
+
 }
 
 
