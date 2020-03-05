af051d5bca9c62b77e5ae38293a60d110a638242
Hafizh <eva_01@yahoo.com>
2020-03-05 12:09:03 +0800

    tukar kepada format code baru at TrainingScreen

--- src/screens/TrainingScreen.js
+++ src/screens/TrainingScreen.js
@@ -1,5 +1,5 @@
 //console.ignoredYellowBox = ['Setting a timer']
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     Image,
     Platform,
@@ -19,7 +19,7 @@ import {
 
 import Constants from 'expo-constants'
 import { LinearGradient } from 'expo-linear-gradient'
-
+import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 import Layout from '../constants/Layout'
 
 import { Ionicons } from '@expo/vector-icons';
@@ -29,55 +29,45 @@ import { Tabs, Tab, ScrollableTab, Drawer, Container, Header, Content, Footer, L
 import { connect } from 'react-redux'
 import * as actionCreator from '../store/actions/action'
 
-class TrainingScreen extends React.PureComponent {
-    static navigationOptions = {
-        header: null,
-    };
-    nav = (screen) => {
-        this.props.navigation.navigate(screen)
+
+const TrainingScreen = (props) => {
+
+    const nav = (screen) => {
+        props.navigation.navigate(screen)
     }
 
-    componentDidMount() {
-        this.props.initiateTraining()
-    }
+    const dispatch = useDispatch()
+    const { trainingArray } = useSelector(state => state.trainingReducer, shallowEqual)
 
-    render() {
+    useEffect(() => {
+        dispatch(actionCreator.initiateTraining())
+    }, []); // empty-array means don't watch for any updates
 
-        const { jwt } = this.props
-        console.log(`jwt ialah ${jwt}`)
-        const uri = `https://lms.bxcess.my/category/training/courses/?bx-token=${jwt}`
 
-        return (
-            <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
-                <WebView source={{ uri }} style={{ flex: 1, backgroundColor: 'transparent' }} />
-                
-            <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0,right:0, flex: 1, flexDirection: 'row', justifyContent: 'center',alignItems:'center' }}>
-                <View style={{ flex: 1,alignSelf:'stretch', justifyContent: 'center', border: 1, borderColor: '#000',alignItems:'center' }}>
-                    <TouchableOpacity onPress={() => this.props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
-                       <View style={{borderWidth:3,borderColor:'#fff',width:Layout.window.width/5}} />
-                        
+    const { jwt } = useSelector(state => state.myAccountReducer, shallowEqual)
+    console.log(`jwt ialah ${jwt}`)
+    const uri = `https://lms.bxcess.my/category/training/courses/?bx-token=${jwt}`
+
+    return (
+        <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
+            <WebView source={{ uri }} style={{ flex: 1, backgroundColor: 'transparent' }} />
+
+            <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, right: 0, flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
+                <View style={{ flex: 1, alignSelf: 'stretch', justifyContent: 'center', border: 1, borderColor: '#000', alignItems: 'center' }}>
+                    <TouchableOpacity onPress={() => props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
+                        <View style={{ borderWidth: 3, borderColor: '#fff', width: Layout.window.width / 5 }} />
+
                     </TouchableOpacity>
                 </View>
-              
+
             </View>
-            </View>
+        </View>
 
-        );
-    }
+    );
+
 }
 
 
-function mapStateToProps(state) {
-    return {
-        trainingArray: state.trainingReducer.trainingArray,
-        jwt: state.myAccountReducer.jwt
 
 
-    }
-}
-function mapDispatchToProps(dispatch) {
-    return {
-        initiateTraining: () => dispatch(actionCreator.initiateTraining()),
-    }
-}
-export default connect(mapStateToProps, mapDispatchToProps)(TrainingScreen)
+export default TrainingScreen
