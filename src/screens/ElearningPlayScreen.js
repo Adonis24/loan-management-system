8f074788579e38b012fdbe6339a30bbea7d5ad89
Hafizh <eva_01@yahoo.com>
2020-03-06 17:09:53 +0800

    remove unecessary component at ElearningPlay

--- src/screens/ElearningPlayScreen.js
+++ src/screens/ElearningPlayScreen.js
@@ -2,17 +2,10 @@
 import React from 'react';
 import {
     Image,
-    Platform,
     ScrollView,
-    StyleSheet,
     Text,
     TouchableOpacity,
     View,
-    Dimensions,
-    TextInput,
-    AsyncStorage,
-    ImageBackground
-
 
 } from 'react-native';
 
@@ -24,9 +17,6 @@ import Layout from '../constants/Layout'
 import { Ionicons } from '@expo/vector-icons';
 import styles from '../styles/styles'
 //import { Drawer, Container, Header, Content, Footer, Left, Right, Body, Title, Subtitle, Button, Icon, Card, CardItem, Text, H2, FooterTab } from 'native-base'
-
-import { connect } from 'react-redux'
-import * as actionCreator from '../store/actions/action'
 
 const ElearningPlayScreen = (props) => {
 
