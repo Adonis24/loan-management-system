c4d2e741ae187790de48a991f392f8621861c2fc
Hafizh <eva_01@yahoo.com>
2020-03-06 17:12:39 +0800

    remove unecessary component at InfoNews

--- src/screens/InfoNewsScreen.js
+++ src/screens/InfoNewsScreen.js
@@ -2,20 +2,13 @@
 import React from 'react';
 import {
     Image,
-    Platform,
     ScrollView,
-    StyleSheet,
     Text,
     TouchableOpacity,
-    View,
-    Dimensions,
-    TextInput,
-    AsyncStorage,
-    ImageBackground
-
+    View
 
 } from 'react-native';
-import { WebView } from 'react-native-webview'
+
 import HTML from 'react-native-render-html'
 
 import Constants from 'expo-constants'
@@ -27,8 +20,6 @@ import { Ionicons } from '@expo/vector-icons';
 import styles from '../styles/styles'
 //import { Drawer, Container, Header, Content, Footer, Left, Right, Body, Title, Subtitle, Button, Icon, Card, CardItem, Text, H2, FooterTab } from 'native-base'
 
-import { connect } from 'react-redux'
-import * as actionCreator from '../store/actions/action'
 
 
 const InfoNewsScreen = (props) => {
