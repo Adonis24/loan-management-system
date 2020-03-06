e3eaf2503ad5a5d5718807882408d0739f0d1d8c
Hafizh <eva_01@yahoo.com>
2020-03-06 17:14:59 +0800

    remove unecessary component at Notification

--- src/screens/NotificationsScreen.js
+++ src/screens/NotificationsScreen.js
@@ -2,31 +2,19 @@
 import React from 'react';
 import {
     Image,
-    Platform,
     ScrollView,
-    StyleSheet,
     Text,
-    TouchableOpacity,
     View,
-    Dimensions,
-    TextInput,
-    AsyncStorage,
-    ImageBackground
-
-
 } from 'react-native';
 
 import Constants from 'expo-constants'
 //import { Constants, LinearGradient, FileSystem } from 'expo'
 
 import Layout from '../constants/Layout'
-import { LinearGradient } from 'expo-linear-gradient'
-import { Ionicons } from '@expo/vector-icons';
 import styles from '../styles/styles'
 //import { Drawer, Container, Header, Content, Footer, Left, Right, Body, Title, Subtitle, Button, Icon, Card, CardItem, Text, H2, FooterTab } from 'native-base'
-import { shallowEqual, useSelector, useDispatch } from 'react-redux'
-import { connect } from 'react-redux'
-import * as actionCreator from '../store/actions/action'
+import { shallowEqual, useSelector} from 'react-redux'
+
 
 
 const NotificationsScreen = (props) => {
