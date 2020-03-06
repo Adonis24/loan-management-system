56cc48be335ac1d38c982dee568857e5428a9ed1
Hafizh <eva_01@yahoo.com>
2020-03-06 17:15:32 +0800

    remove unecessary component at PopupScore

--- src/screens/PopupScoreScreen.js
+++ src/screens/PopupScoreScreen.js
@@ -1,33 +1,12 @@
 //console.ignoredYellowBox = ['Setting a timer']
 import React from 'react';
-import {
-    Image,
-    Platform,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-    Dimensions,
-    TextInput,
-    AsyncStorage,
-    ImageBackground
-
-
-} from 'react-native';
-
-import Constants from 'expo-constants'
-//import { Constants, LinearGradient, FileSystem } from 'expo'
+import {  Text,TouchableOpacity,View} from 'react-native';
 
 import Layout from '../constants/Layout'
 
 import { Ionicons } from '@expo/vector-icons';
 import styles from '../styles/styles'
 //import { Drawer, Container, Header, Content, Footer, Left, Right, Body, Title, Subtitle, Button, Icon, Card, CardItem, Text, H2, FooterTab } from 'native-base'
-
-import { connect } from 'react-redux'
-import * as actionCreator from '../store/actions/action'
-
 
    const PopupScoreScreen = (props) => {
     
