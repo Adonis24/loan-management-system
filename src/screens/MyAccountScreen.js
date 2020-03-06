4559405b429fbc1462ab67d4913d66857a31f484
Hafizh <eva_01@yahoo.com>
2020-03-06 11:49:45 +0800

    fix back button at MyAccountScreen

--- src/screens/MyAccountScreen.js
+++ src/screens/MyAccountScreen.js
@@ -91,7 +91,7 @@ const MyAccountScreen = (props) => {
                         style={{ flex: 1, alignItems: 'center', justifyContent: 'space-between' }}>
                         <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
                             <View style={{ flex: 1, marginLeft: 10, justifyContent: 'center', border: 1, borderColor: '#000' }}>
-                                <TouchableOpacity onPress={() => navigation.goBack()} >
+                                <TouchableOpacity onPress={() => props.navigation.goBack()} >
                                     <Ionicons name={Platform.OS === 'ios' ? 'ios-arrow-down' : 'ios-arrow-back'} size={32} color={'#fff'} />
                                 </TouchableOpacity>
                             </View>
