5568f7c77252bf50445ba392e193443927b9ed86
Hafizh <eva_01@yahoo.com>
2020-03-06 17:03:50 +0800

    remove unecessary component at BizLicensing

--- src/screens/BizLicensingScreen.js
+++ src/screens/BizLicensingScreen.js
@@ -2,31 +2,18 @@
 import React from 'react';
 import {
     Image,
-    Platform,
-    ScrollView,
-    StyleSheet,
     Text,
     TouchableOpacity,
-    View,
-    Dimensions,
-    TextInput,
-    AsyncStorage,
-    ImageBackground
-
-
+    View
 } from 'react-native';
 
 import Constants from 'expo-constants'
-import { LinearGradient } from 'expo-linear-gradient'
 
 import Layout from '../constants/Layout'
 
 import { Ionicons } from '@expo/vector-icons';
 import styles from '../styles/styles'
-import { Tabs, Tab, ScrollableTab, Drawer, Container, Header, Content, Footer, Left, Right, Body, Title, Subtitle, Button, Icon, Card, CardItem, H2, FooterTab } from 'native-base'
 
-import { connect } from 'react-redux'
-import * as actionCreator from '../store/actions/action'
 
 
 const BizLicensingScreen = (props) => {
