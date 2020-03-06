0e4cd0c9183efda5aa013168a5d4d41250b0aec4
Hafizh <eva_01@yahoo.com>
2020-03-06 17:10:52 +0800

    remove unecessary component at Grant

--- src/screens/GrantScreen.js
+++ src/screens/GrantScreen.js
@@ -2,17 +2,9 @@
 import React from 'react';
 import {
     Image,
-    Platform,
-    ScrollView,
-    StyleSheet,
     Text,
     TouchableOpacity,
     View,
-    Dimensions,
-    TextInput,
-    AsyncStorage,
-    ImageBackground,
-
 
 } from 'react-native';
 
@@ -23,10 +15,7 @@ import Layout from '../constants/Layout'
 
 import { Ionicons } from '@expo/vector-icons';
 import styles from '../styles/styles'
-import { Tabs, Tab, ScrollableTab, Drawer, Container, Header, Content, Footer, Left, Right, Body, Title, Subtitle, Button, Icon, Card, CardItem, H2, FooterTab } from 'native-base'
 
-import { connect } from 'react-redux'
-import * as actionCreator from '../store/actions/action'
 
 
 const GrantScreen = (props) => {
