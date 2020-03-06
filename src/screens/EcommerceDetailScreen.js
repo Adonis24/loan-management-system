77f45b34bdc58f334449270f2966014300c1dea0
Hafizh <eva_01@yahoo.com>
2020-03-06 17:07:59 +0800

    remove unecessary component at EcommerceDetail

--- src/screens/EcommerceDetailScreen.js
+++ src/screens/EcommerceDetailScreen.js
@@ -2,18 +2,9 @@
 import React from 'react';
 import {
     Image,
-    Platform,
-    ScrollView,
-    StyleSheet,
     Text,
     TouchableOpacity,
-    View,
-    Dimensions,
-    TextInput,
-    AsyncStorage,
-    ImageBackground,
-
-
+    View
 } from 'react-native';
 
 import Constants from 'expo-constants'
@@ -23,10 +14,7 @@ import Layout from '../constants/Layout'
 
 import { Ionicons } from '@expo/vector-icons';
 import styles from '../styles/styles'
-import { Tabs, Tab, ScrollableTab, Drawer, Container, Header, Content, Footer, Left, Right, Body, Title, Subtitle, Button, Icon, Card, CardItem, H2, FooterTab } from 'native-base'
 
-import { connect } from 'react-redux'
-import * as actionCreator from '../store/actions/action'
 
 const EcommerceDetailScreen = (props) => {
 
