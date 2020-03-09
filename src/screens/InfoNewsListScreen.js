6a4d2fef3215553a6d6e884ebca89c6344b078cc
Hafizh <eva_01@yahoo.com>
2020-03-09 16:13:19 +0800

    change InfoNewsListScreen to LayoutB

--- src/screens/InfoNewsListScreen.js
+++ src/screens/InfoNewsListScreen.js
@@ -20,6 +20,7 @@ import styles from '../styles/styles'
 import { Tabs, Tab, ScrollableTab } from 'native-base'
 
 import * as actionCreator from '../store/actions/action'
+import LayoutB from '../Layout/Layout B';
 
 
 const InfoNewsListScreen = (props) => {
@@ -30,7 +31,7 @@ const InfoNewsListScreen = (props) => {
 
     const hafiz = (item) => {
         console.log(`item ialah : ${JSON.stringify(item)}`)
-        props.navigation.navigate('InfoNews', { item:JSON.stringify(item) })
+        props.navigation.navigate('InfoNews', { item: JSON.stringify(item) })
     }
 
     // props.nav('InfoNews', { item })
@@ -45,54 +46,37 @@ const InfoNewsListScreen = (props) => {
     }, []); // empty-array means don't watch for any updates
 
     return (
-        <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
-            <View style={{ flex: 1, justifyContent: 'space-between' }}>
-                <View style={{ alignItems: 'flex-end' }}>
-                    <Image source={require('../assets/images/topRight.png')} style={{ width: 140, height: 130 }} resizeMode={'contain'} />
-                </View>
-                <View style={{ alignItems: 'flex-start' }}>
-                    <Image source={require('../assets/images/bottomLeft.png')} style={{ width: 79, height: 143 }} resizeMode={'contain'} />
-                </View>
-            </View>
-            <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, bottom: 0, right: 0, }}>
-                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <View style={{ flex: 1, marginLeft: 10, justifyContent: 'center', border: 1, borderColor: '#000' }}>
-                        <TouchableOpacity onPress={() => props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
-                            <Ionicons name='ios-arrow-back' size={32} />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ flex: 4, marginTop: 5, marginBottom: 5, paddingTop: 5, paddingBottom: 5 }}>
-                        <View style={[{ backgroundColor: '#fff', marginLeft: Layout.window.width / 3, borderBottomLeftRadius: 20, borderTopLeftRadius: 20, borderWidth: 1, borderRightWidth: 0, borderColor: 'lightgrey', flexDirection: 'row', elevation: 2, justifyContent: 'flex-start' }]}>
-                            <Image source={require('../assets/images/news.png')} style={{ width: Layout.window.height / 15, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                            <Text style={[styles.default, { alignSelf: 'center', fontSize: 18, fontWeight: "bold" }]} numberOfLines={1} ellipsizeMode={'tail'}>News</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={{ flex: 7, justifyContent: 'center', alignItems: 'center' }}>
-                    {newsArray && newsArray.length > 0 ? <Tabs tabBarBackgroundColor={'transparent'} tabContainerStyle={{ backgroundColor: 'transparent' }} tabBarTextStyle={[styles.textDefault, { color: '#000' }]} tabBarUnderlineStyle={{ backgroundColor: 'lightgrey' }} renderTabBar={() => <ScrollableTab />}>
-                        <Tab heading="Latest">
-                            <Latest nav={nav} hafiz={hafiz} newsArray={newsArray} />
-                        </Tab>
-                        <Tab heading="Popular">
-                            <Popular nav={nav} hafiz={hafiz} newsArray={newsArray} />
-                        </Tab>
-                    </Tabs> : <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
-                            <Text style={[styles.textDefault, { textAlign: 'left', margin: 10, alignSelf: 'flex-start', fontSize: 14, color: 'lightgrey' }]}>Please check back for latest info soon</Text>
-                            {/*                             
+
+        < LayoutB
+            title={'News'}
+            screenType='form'
+            navigation={props.navigation}
+            imageUri={require('../assets/images/news.png')}
+        >
+            <View style={{ flex: 7, justifyContent: 'center', alignItems: 'center' }}>
+                {newsArray && newsArray.length > 0 ? <Tabs tabBarBackgroundColor={'transparent'} tabContainerStyle={{ backgroundColor: 'transparent' }} tabBarTextStyle={[styles.textDefault, { color: '#000' }]} tabBarUnderlineStyle={{ backgroundColor: 'lightgrey' }} renderTabBar={() => <ScrollableTab />}>
+                    <Tab heading="Latest">
+                        <Latest nav={nav} hafiz={hafiz} newsArray={newsArray} />
+                    </Tab>
+                    <Tab heading="Popular">
+                        <Popular nav={nav} hafiz={hafiz} newsArray={newsArray} />
+                    </Tab>
+                </Tabs> : <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+                        <Text style={[styles.textDefault, { textAlign: 'left', margin: 10, alignSelf: 'flex-start', fontSize: 14, color: 'lightgrey' }]}>Please check back for latest info soon</Text>
+                        {/*                             
                                 <Image source={require('../assets/images/bizlicensing.png')} style={{ flex:1,width: undefined, height: Layout.window.height / 1.5, alignSelf: 'center',opacity:0.3, borderWidth:1,borderColor:'#000' }} resizeMode={'contain'} /> */}
 
-                        </View>
-                    }
-                </View>
+                    </View>
+                }
             </View>
-        </View>
+        </ LayoutB>
     );
 
 }
 
 const Latest = (props) => {
     return (
-        <ScrollView contentContainerStyle={{ width: Layout.window.width }} style={{ width: Layout.window.width, padding: 20 }}>
+        <View style={{ width: Layout.window.width, padding: 20 }}>
             {props.newsArray &&
                 <FlatList
                     data={props.newsArray}
@@ -105,7 +89,7 @@ const Latest = (props) => {
                         </TouchableOpacity>
 
                     )} />}
-        </ScrollView>
+        </View>
     )
 
 }
@@ -115,7 +99,7 @@ const Latest = (props) => {
 const Popular = (props) => {
 
     return (
-        <ScrollView style={{ padding: 20 }}>
+        <View style={{ width: Layout.window.width, padding: 20 }}>
             {props.newsArray &&
                 <FlatList
                     data={props.newsArray}
@@ -129,7 +113,7 @@ const Popular = (props) => {
                         </View>
                     )} />
             }
-        </ScrollView>
+        </View>
     )
 
 }
