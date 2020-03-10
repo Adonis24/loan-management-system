3c3f91a484e6e48c15e3d9f4935ba320de9820b3
Hafizh <eva_01@yahoo.com>
2020-03-10 17:37:38 +0800

    add layout B to NotificationScreen

--- src/screens/NotificationsScreen.js
+++ src/screens/NotificationsScreen.js
@@ -9,11 +9,12 @@ import {
 
 import Constants from 'expo-constants'
 //import { Constants, LinearGradient, FileSystem } from 'expo'
+import LayoutB from '../Layout/Layout B';
 
 import Layout from '../constants/Layout'
 import styles from '../styles/styles'
 //import { Drawer, Container, Header, Content, Footer, Left, Right, Body, Title, Subtitle, Button, Icon, Card, CardItem, Text, H2, FooterTab } from 'native-base'
-import { shallowEqual, useSelector} from 'react-redux'
+import { shallowEqual, useSelector } from 'react-redux'
 
 
 
@@ -23,55 +24,32 @@ const NotificationsScreen = (props) => {
 
 
     return (
-        <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
+        < LayoutB
+            title={'Notifications'}
+            screenType='logo'
+            navigation={props.navigation}
+            imageUri={require('../assets/images/profile.png')}
+        >
 
-            <View style={{ flex: 1, justifyContent: 'space-between' }}>
-                <View style={{ alignItems: 'flex-end' }}>
-                    <Image source={require('../assets/images/topRight.png')} style={{ width: 140, height: 130 }} resizeMode={'contain'} />
-                </View>
-                {/* <View style={{ alignItems: 'flex-start' }}>
-                    <Image source={require('../assets/images/bottomLeft.png')} style={{ width: 79, height: 143 }} resizeMode={'contain'} />
-                </View> */}
-            </View>
-            <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, bottom: 0, right: 0, }}>
-                {/* HEADER */}
-                <View style={{ flex: 1 }}>
-                    <View style={{ flex: 1, marginLeft: -10 }}>
-                        <Image source={require('../assets/images/logo.png')} style={{ width: Layout.window.width / 3, height: undefined, flex: 1 }} resizeMode='contain' />
-
-                    </View>
-                    <View style={{ flex: 1, marginTop: 5, marginBottom: 5, paddingTop: 5, paddingBottom: 5, flexDirection: 'row' }}>
-                        <View style={{ flex: 5, flexDirection: 'row' }}>
-
-                        </View>
-                        <View style={[{ backgroundColor: '#fff', flex: 4, borderBottomLeftRadius: 20, borderTopLeftRadius: 20, borderWidth: 1, borderRightWidth: 0, borderColor: 'lightgrey', paddingLeft: 5, flexDirection: 'row', elevation: 2 }]}>
-
-                            <Image source={require('../assets/images/profile.png')} style={{ width: Layout.window.width / 10, height: undefined, }} resizeMode={'contain'} />
-                            <Text style={[styles.textDefault, { fontSize: 18, fontWeight: "bold", paddingLeft: 5, }]} numberOfLines={1} ellipsizeMode={'tail'}>Notifications</Text>
-
-                        </View>
-                    </View>
-                </View>
-                {/* CONTENT AREA */}
-                <View style={{ flex: 4 }}>
-                    <ScrollView contentStyle={{ padding: 10 }} >
-                        {notificationList && notificationList.length > 0 ?
-                            <View style={[styles.shadow, { backgroundColor: '#fff', flex: 1, alignSelf: 'stretch', borderRadius: 20, marginLeft: 10, marginRight: 10, borderWidth: 1, borderColor: '#ddd', padding: 10, marginBottom: 20, justifyContent: 'space-between' }]}>
-                                {/* <View style={[{ flex: 1, marginLeft: 10, padding: 2, alignSelf: 'stretch' }]}>
+            {/* CONTENT AREA */}
+            
+                <ScrollView contentStyle={{ padding: 10 }} >
+                    {notificationList && notificationList.length > 0 ?
+                        <View style={[styles.shadow, { backgroundColor: '#fff', flex: 1, alignSelf: 'stretch', borderRadius: 20, marginLeft: 10, marginRight: 10, borderWidth: 1, borderColor: '#ddd', padding: 10, marginBottom: 20, justifyContent: 'space-between' }]}>
+                            {/* <View style={[{ flex: 1, marginLeft: 10, padding: 2, alignSelf: 'stretch' }]}>
                                     <Image source={require('../assets/images/girl.png')} style={{ flex: 1, height: Layout.window.height / 10, width: undefined, borderWidth: 1, borderColor: 'lightgrey' }} resizeMode='cover' />
                                 </View> */}
-                                <Text style={[styles.caption, { margin: 5, alignSelf: 'flex-end', textAlign: 'right' }]}>20/12/2019</Text>
-                                <Text numberOfLines={3} ellipsizeMode={'tail'} style={[styles.textDefault, { margin: 5, alignSelf: 'flex-start', textAlign: 'left' }]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper luctus porttitor. Sed a venenatis ipsum, eget suscipit sapien. Aliquam ornare sagittis felis et tempor. </Text>
-                            </View> :
-                            <View style={[styles.shadow, { backgroundColor: '#fff', flex: 1, alignSelf: 'stretch', borderRadius: 20, marginLeft: 10, marginRight: 10, borderWidth: 1, borderColor: '#ddd', padding: 10, marginBottom: 20, justifyContent: 'space-between' }]}>
-                                {/* <View style={[{ flex: 1, marginLeft: 10, padding: 2, alignSelf: 'stretch' }]}>
+                            <Text style={[styles.caption, { margin: 5, alignSelf: 'flex-end', textAlign: 'right' }]}>20/12/2019</Text>
+                            <Text numberOfLines={3} ellipsizeMode={'tail'} style={[styles.textDefault, { margin: 5, alignSelf: 'flex-start', textAlign: 'left' }]}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse ullamcorper luctus porttitor. Sed a venenatis ipsum, eget suscipit sapien. Aliquam ornare sagittis felis et tempor. </Text>
+                        </View> :
+                        <View style={[styles.shadow, { backgroundColor: '#fff', flex: 1, alignSelf: 'stretch', borderRadius: 20, marginLeft: 10, marginRight: 10, borderWidth: 1, borderColor: '#ddd', padding: 10, marginBottom: 20, justifyContent: 'space-between' }]}>
+                            {/* <View style={[{ flex: 1, marginLeft: 10, padding: 2, alignSelf: 'stretch' }]}>
                             <Image source={require('../assets/images/girl.png')} style={{ flex: 1, height: Layout.window.height / 10, width: undefined, borderWidth: 1, borderColor: 'lightgrey' }} resizeMode='cover' />
                         </View> */}
-                                <Text style={[styles.caption, { margin: 5, alignSelf: 'flex-start', color: 'lightgrey' }]}>No notification yet</Text>
-                            </View>}
-                    </ScrollView>
-                </View>
-            </View>
+                            <Text style={[styles.caption, { margin: 5, alignSelf: 'flex-start', color: 'lightgrey' }]}>No notification yet</Text>
+                        </View>}
+                </ScrollView>
+           
             {/* <PopupScoreScreen /> */}
 
             {/* <View style={{ position: 'absolute', top: Constants.statusBarHeight, right: 0 }}>
@@ -79,8 +57,8 @@ const NotificationsScreen = (props) => {
                         <Image source={require('../assets/images/qr.png')} style={{ width: 50, height: 50 }} />
                     </TouchableOpacity>
                 </View> */}
-        </View>
 
+        </ LayoutB>
     );
 
 }
