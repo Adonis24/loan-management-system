1603f45e39b691f58a38b6339650fbbec2e43e01
Hafizh <eva_01@yahoo.com>
2020-03-10 17:36:14 +0800

    add layout B to InfoScreen

--- src/screens/InfoScreen.js
+++ src/screens/InfoScreen.js
@@ -10,6 +10,7 @@ import {
 
 import Constants from 'expo-constants'
 //import { Constants, LinearGradient, FileSystem } from 'expo'
+import LayoutB from '../Layout/Layout B';
 
 import Layout from '../constants/Layout'
 
@@ -43,29 +44,13 @@ const InfoScreen = (props) => {
    
 
     return (
-        <View style={{ flex: 1, paddingTop: Constants.statusBarHeight }}>
-            <View style={{ flex: 1, justifyContent: 'space-between' }}>
-                <View style={{ alignItems: 'flex-end' }}>
-                    <Image source={require('../assets/images/topRight.png')} style={{ width: 140, height: 130 }} resizeMode={'contain'} />
-                </View>
-                {/* <View style={{ alignItems: 'flex-start' }}>
-                        <Image source={require('../assets/images/bottomLeft.png')} style={{ width: 79, height: 143 }} resizeMode={'contain'} />
-                    </View> */}
-            </View>
-            <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, bottom: 0, right: 0, }}>
-                <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <View style={{ flex: 1, marginLeft: 10, justifyContent: 'center', border: 1, borderColor: '#000' }}>
-                        <TouchableOpacity onPress={() => props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
-                            <Ionicons name='ios-arrow-back' size={32} />
-                        </TouchableOpacity>
-                    </View>
-                    <View style={{ flex: 4, marginTop: 5, marginBottom: 5, paddingTop: 5, paddingBottom: 5 }}>
-                        <View style={[{ backgroundColor: '#fff', marginLeft: Layout.window.width / 3, borderBottomLeftRadius: 20, borderTopLeftRadius: 20, borderWidth: 1, borderRightWidth: 0, borderColor: 'lightgrey', flexDirection: 'row', elevation: 2, justifyContent: 'flex-start' }]}>
-                            <Image source={require('../assets/images/e-info.png')} style={{ width: Layout.window.height / 15, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                            <Text style={[styles.default, { alignSelf: 'center', fontSize: 18, fontWeight: "bold" }]} numberOfLines={1} ellipsizeMode={'tail'}>e-info</Text>
-                        </View>
-                    </View>
-                </View>
+        < LayoutB
+        title={'e-info'}
+        screenType='NO'
+        navigation={props.navigation}
+        imageUri={require('../assets/images/e-info.png')}
+    >
+        
                 {/** Content */}
                 <View style={{ flex: 7, justifyContent: 'center', alignItems: 'center', padding: 10, backgroundColor: 'transparent' }}>
                     {einfosArray && einfosArray.length > 0 ? <Accordion style={{ alignSelf: 'stretch', backgroundColor: 'transparent' }} dataArray={einfosArray} icon="add" expandedIcon="remove" />
@@ -75,8 +60,7 @@ const InfoScreen = (props) => {
                                 <Image source={require('../assets/images/bizlicensing.png')} style={{ flex:1,width: undefined, height: Layout.window.height / 1.5, alignSelf: 'center',opacity:0.3, borderWidth:1,borderColor:'#000' }} resizeMode={'contain'} /> */}
 
                         </View>}</View>
-            </View>
-        </View>
+        </ LayoutB>
     );
 
 }
