b275b52b9bc0aba4054a43bc3d0e9b14f360c9c1
Syahrizan Ali Hassan <syahrizan@fintechcapital.co>
2020-03-12 13:25:00 +0800

    remove accordian -nativebase

--- src/screens/InfoScreen.js
+++ src/screens/InfoScreen.js
@@ -1,5 +1,5 @@
 //console.ignoredYellowBox = ['Setting a timer']
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     Image,
     Text,
@@ -7,16 +7,15 @@ import {
     View
 
 } from 'react-native';
-
+import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 import Constants from 'expo-constants'
 //import { Constants, LinearGradient, FileSystem } from 'expo'
 import LayoutB from '../Layout/LayoutB';
 
 import Layout from '../constants/Layout'
 
-import { Ionicons } from '@expo/vector-icons';
+
 import styles from '../styles/styles'
-import { Accordion} from 'native-base'
 
 import * as actionCreator from '../store/actions/action'
 
@@ -29,37 +28,33 @@ const dataArray = [
 
 const InfoScreen = (props) => {
 
-   const nav = (screen, item) => {
+    const nav = (screen, item) => {
         props.navigation.navigate(screen, { item })
     }
     const dispatch = useDispatch()
-    const {einfosArray} = useSelector(state => state.bizInfoReducer, shallowEqual)
+    const { einfosArray } = useSelector(state => state.bizInfoReducer, shallowEqual)
 
     useEffect(() => {
-
         dispatch(actionCreator.intitiateEinfo())
-        
-    }, []); // empty-array means don't watch for any updates
 
-   
+    }, []); // empty-array means don't watch for any updates
 
     return (
         < LayoutB
-        title={'e-info'}
-        screenType='NO'
-        navigation={props.navigation}
-        imageUri={require('../assets/images/e-info.png')}
-    >
-        
-                {/** Content */}
-                <View style={{ flex: 7, justifyContent: 'center', alignItems: 'center', padding: 10, backgroundColor: 'transparent' }}>
-                    {einfosArray && einfosArray.length > 0 ? <Accordion style={{ alignSelf: 'stretch', backgroundColor: 'transparent' }} dataArray={einfosArray} icon="add" expandedIcon="remove" />
-                        : <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
-                            <Text style={[styles.textDefault, { textAlign: 'left', margin: 10, alignSelf: 'flex-start', fontSize: 14, color: 'lightgrey' }]}>Please check back for latest info soon</Text>
-                            {/*                             
+            title={'e-info'}
+            screenType='NO'
+            navigation={props.navigation}
+            imageUri={require('../assets/images/e-info.png')}
+        >
+
+            {/** Content */}
+            <View style={{ flex: 7, justifyContent: 'center', alignItems: 'center', padding: 10, backgroundColor: 'transparent' }}>
+                <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+                    <Text style={[styles.textDefault, { textAlign: 'left', margin: 10, alignSelf: 'flex-start', fontSize: 14, color: 'lightgrey' }]}>Please check back for latest info soon</Text>
+                    {/*                             
                                 <Image source={require('../assets/images/bizlicensing.png')} style={{ flex:1,width: undefined, height: Layout.window.height / 1.5, alignSelf: 'center',opacity:0.3, borderWidth:1,borderColor:'#000' }} resizeMode={'contain'} /> */}
 
-                        </View>}</View>
+                </View></View>
         </ LayoutB>
     );
 
