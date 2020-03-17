a1c8295410b2428d1d902cf63fb506be9825d714
Hafizh <eva_01@yahoo.com>
2020-03-17 15:26:49 +0800

    implement netinfo to SignupPersonal

--- src/screens/SignupPersonalScreen.js
+++ src/screens/SignupPersonalScreen.js
@@ -53,7 +53,7 @@ const SignupPersonalScreen = (props) => {
 
     const dispatch = useDispatch()
     const publicToken = useSelector(state => state.registrationReducer, shallowEqual)
-    
+    const { isConnected, isInternetReachable, type } = useSelector(state => state.netInfoReducer, shallowEqual)
     publicToken && console.log(`registration token ialah :${JSON.stringify(publicToken)} `)
 
 
@@ -149,8 +149,8 @@ const SignupPersonalScreen = (props) => {
                                     </View>
 
                                     <View style={{ flexDirection: 'row', margin: 5 }}>
-                                        <TouchableOpacity disabled={!FormikProps.isValid} onPress={() => FormikProps.handleSubmit()} style={styles.box}>
-                                            <LinearGradient colors={FormikProps.isValid ? ['#4DCB3E', '#269B1D',] : ['rgba(77,203,62,0.5)', 'rgba(38,155,29,0.5)',]} style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, borderRadius: 15, justifyContent: 'center' }}>
+                                        <TouchableOpacity disabled={!FormikProps.isValid || !isInternetReachable} onPress={() => FormikProps.handleSubmit()} style={styles.box}>
+                                            <LinearGradient colors={(FormikProps.isValid && isInternetReachable)  ? ['#4DCB3E', '#269B1D',] : ['rgba(77,203,62,0.5)', 'rgba(38,155,29,0.5)',]} style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, borderRadius: 15, justifyContent: 'center' }}>
                                                 <Text style={[styles.textDefault, { color: '#fff' }]}>Next</Text>
                                             </LinearGradient>
                                         </TouchableOpacity>
