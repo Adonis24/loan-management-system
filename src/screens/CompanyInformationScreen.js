ea5686cc1c0e69d55d1c3db06263fcd1040b019b
Hafizh <eva_01@yahoo.com>
2020-03-17 15:23:43 +0800

    implement netinfo to companyInfo

--- src/screens/CompanyInformationScreen.js
+++ src/screens/CompanyInformationScreen.js
@@ -67,7 +67,7 @@ const CompanyInformationScreen = (props) => {
 
     const [iosDatePickerShow, setIosDatePickerShow] = useState(false)
     const [chosenDate, setChosenDate] = useState(new Date())
-
+    const { isConnected, isInternetReachable, type } = useSelector(state => state.netInfoReducer, shallowEqual)
 
     //proceed && props.navigation.navigate('CompanyContactInformation')
 
@@ -211,8 +211,8 @@ const CompanyInformationScreen = (props) => {
                                             />
                                         )}
                                         <View style={{ flexDirection: 'row', margin: 5 }}>
-                                            <TouchableOpacity disabled={!FormikProps.isValid} onPress={() => FormikProps.handleSubmit()} style={styles.box}>
-                                                <LinearGradient colors={FormikProps.isValid ? ['#4DCB3E', '#269B1D',] : ['rgba(77,203,62,0.5)', 'rgba(38,155,29,0.5)',]} style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, borderRadius: 15, justifyContent: 'center' }}>
+                                            <TouchableOpacity disabled={!FormikProps.isValid || !isInternetReachable} onPress={() => FormikProps.handleSubmit()} style={styles.box}>
+                                                <LinearGradient colors={(FormikProps.isValid && isInternetReachable) ? ['#4DCB3E', '#269B1D',] : ['rgba(77,203,62,0.5)', 'rgba(38,155,29,0.5)',]} style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, borderRadius: 15, justifyContent: 'center' }}>
                                                     <Text style={[styles.textDefault, { color: '#fff' }]}>Next</Text>
                                                 </LinearGradient>
                                             </TouchableOpacity>
