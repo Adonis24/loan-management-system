8c75e3ec840384a2ed651f7b9318d80900428336
Hafizh <eva_01@yahoo.com>
2020-03-19 13:16:57 +0800

    add layout A and immplement show logo at DeclarationDigitalSign

--- src/screens/DeclarationDigitalSignScreen.js
+++ src/screens/DeclarationDigitalSignScreen.js
@@ -1,5 +1,5 @@
 //console.ignoredYellowBox = ['Setting a timer']
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Image,
     Text,
@@ -16,7 +16,7 @@ import { Formik } from 'formik';
 import * as Yup from 'yup';
 import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 import Constants from 'expo-constants'
-
+import { keyboardBeingDisplay, keyboardBeingClose } from '../components/handleKeyboard'
 
 import Layout from '../constants/Layout'
 import { LinearGradient } from 'expo-linear-gradient'
@@ -25,7 +25,7 @@ import styles from '../styles/styles'
 import { CustomTextInput } from '../components/Custom'
 
 import * as actionCreator from '../store/actions/action'
-
+import LayoutA from '../Layout/LayoutA';
 
 
 const validationSchema = Yup.object().shape({
@@ -74,125 +74,125 @@ const DeclarationDigitalSignScreen = (props) => {
 
     //const { declareSign, declareName, declarePosition, declareStamp, declareDate, } = useSelector(state => state.companyInformationReducer, shallowEqual)
 
+    useEffect(() => {
+        const open = () => setshowLogo(false)
+        const off = () => setshowLogo(true)
+
+        keyboardBeingDisplay(open)
+        keyboardBeingClose(off)
+    }, []); // empty-array means don't watch for any updates
+
+    const [showLogo, setshowLogo] = useState(true)
+
 
 
     return (
-        <View style={styles.container}>
-            <View style={{ flex: 1, justifyContent: 'space-between' }}>
-                <View style={{ alignItems: 'flex-start' }}><Image source={require('../assets/images/topLeft.png')} style={{ width: 79, height: 120 }} /></View>
-                <View style={{ alignItems: 'flex-end' }}><Image source={require('../assets/images/bottomRight.png')} style={{ width: 106, height: 92 }} /></View>
+        <LayoutA>
 
-            </View>
-            <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, }}>
-                <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Formik
+                validateOnMount
+                initialValues={{}}
 
-                    <Formik
-                        validateOnMount
-                        initialValues={{}}
+                onSubmit={async (values, actions) => {
+                    console.log(`values formik ialah ${JSON.stringify(values)}`)
+                    await setDeclarationSign(values)
+                    DeclarationSign()
+                    //props.navigation.navigate('CompanyInfoSuccess')
+                    actions.setSubmitting(false)
+                }
+                }
+                validationSchema={validationSchema}
+            >
+                {FormikProps => {
 
-                        onSubmit={async (values, actions) => {
-                            console.log(`values formik ialah ${JSON.stringify(values)}`)
-                            await setDeclarationSign(values)
-                            DeclarationSign()
-                            //props.navigation.navigate('CompanyInfoSuccess')
-                            actions.setSubmitting(false)
-                        }
-                        }
-                        validationSchema={validationSchema}
-                    >
-                        {FormikProps => {
+                    const { declareSign, declareName, declarePosition, declareStamp } = FormikProps.values
 
-                            const { declareSign, declareName, declarePosition, declareStamp } = FormikProps.values
+                    const declareSignError = FormikProps.errors.declareSign
+                    const declareSignTouched = FormikProps.touched.declareSign
 
-                            const declareSignError = FormikProps.errors.declareSign
-                            const declareSignTouched = FormikProps.touched.declareSign
+                    const declareNameError = FormikProps.errors.declareName
+                    const declareNameTouched = FormikProps.touched.declareName
 
-                            const declareNameError = FormikProps.errors.declareName
-                            const declareNameTouched = FormikProps.touched.declareName
+                    const declarePositionError = FormikProps.errors.declarePosition
+                    const declarePositionTouched = FormikProps.touched.declarePosition
 
-                            const declarePositionError = FormikProps.errors.declarePosition
-                            const declarePositionTouched = FormikProps.touched.declarePosition
-
-                            const declareStampError = FormikProps.errors.declareStamp
-                            const declareStampTouched = FormikProps.touched.declareStamp
+                    const declareStampError = FormikProps.errors.declareStamp
+                    const declareStampTouched = FormikProps.touched.declareStamp
 
 
 
 
-                            return (
+                    return (
 
-                                <View style={{ width: Layout.window.width * 0.8, justifyContent: 'center', alignItems: 'center' }}>
-                                    <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.7 }} resizeMode={'contain'} />
-                                    <Text style={[styles.textDefault, { margin: 5, fontWeight: 'bold' }]}>REGISTRATION</Text>
-                                    {/* <Image source={require('../assets/images/1.png')} style={{ height: 50, width: 200, margin: 5 }} resizeMode={'stretch'} /> */}
-                                    <Text style={[styles.textDefault, { margin: 5, color: 'darkblue' }]}>Details of Connected Party</Text>
+                        <View style={{ width: Layout.window.width * 0.8, justifyContent: 'center', alignItems: 'center' }}>
+                            {showLogo && <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.7 }} resizeMode={'contain'} />}
+                            <Text style={[styles.textDefault, { margin: 5, fontWeight: 'bold' }]}>REGISTRATION</Text>
+                            {/* <Image source={require('../assets/images/1.png')} style={{ height: 50, width: 200, margin: 5 }} resizeMode={'stretch'} /> */}
+                            <Text style={[styles.textDefault, { margin: 5, color: 'darkblue' }]}>Details of Connected Party</Text>
 
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/user.png')}
-                                        value={declareSign}
-                                        handleChange={FormikProps.handleChange(`declareSign`)}
-                                        handleBlur={FormikProps.handleBlur(`declareSign`)}
-                                        touched={declareSignTouched}
-                                        error={declareSignError}
-                                        placeholder={'Authorized Personal Digital Sign'}
+                            <CustomTextInput
+                                imageUri={require('../assets/images/user.png')}
+                                value={declareSign}
+                                handleChange={FormikProps.handleChange(`declareSign`)}
+                                handleBlur={FormikProps.handleBlur(`declareSign`)}
+                                touched={declareSignTouched}
+                                error={declareSignError}
+                                placeholder={'Authorized Personal Digital Sign'}
 
-                                    />
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/mykad.png')}
-                                        value={declareName}
-                                        handleChange={FormikProps.handleChange(`declareName`)}
-                                        handleBlur={FormikProps.handleBlur(`declareName`)}
-                                        touched={declareNameTouched}
-                                        error={declareNameError}
-                                        placeholder={'Name'}
-                                        keyboardType={'default'}
-                                    />
+                            />
+                            <CustomTextInput
+                                imageUri={require('../assets/images/mykad.png')}
+                                value={declareName}
+                                handleChange={FormikProps.handleChange(`declareName`)}
+                                handleBlur={FormikProps.handleBlur(`declareName`)}
+                                touched={declareNameTouched}
+                                error={declareNameError}
+                                placeholder={'Name'}
+                                keyboardType={'default'}
+                            />
 
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/mykad.png')}
-                                        value={declarePosition}
-                                        handleChange={FormikProps.handleChange(`declarePosition`)}
-                                        handleBlur={FormikProps.handleBlur(`declarePosition`)}
-                                        touched={declarePositionTouched}
-                                        error={declarePositionError}
-                                        placeholder={'Position'}
-                                        keyboardType={'numbers-and-punctuation'}
-                                    />
+                            <CustomTextInput
+                                imageUri={require('../assets/images/mykad.png')}
+                                value={declarePosition}
+                                handleChange={FormikProps.handleChange(`declarePosition`)}
+                                handleBlur={FormikProps.handleBlur(`declarePosition`)}
+                                touched={declarePositionTouched}
+                                error={declarePositionError}
+                                placeholder={'Position'}
+                                keyboardType={'numbers-and-punctuation'}
+                            />
 
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/user.png')}
-                                        value={declareStamp}
-                                        handleChange={FormikProps.handleChange(`declareStamp`)}
-                                        handleBlur={FormikProps.handleBlur(`declareStamp`)}
-                                        touched={declareStampTouched}
-                                        error={declareStampError}
-                                        placeholder={'Stamp'}
-                                        keyboardType={'default'}
-                                    />
-
-
-
-                                    <View style={{ flexDirection: 'row', margin: 5 }}>
-                                        <TouchableOpacity disabled={!FormikProps.isValid || !isInternetReachable} onPress={() => FormikProps.handleSubmit()} style={styles.box}>
-                                            <LinearGradient colors={(FormikProps.isValid && isInternetReachable) ? ['#4DCB3E', '#269B1D',] : ['rgba(77,203,62,0.5)', 'rgba(38,155,29,0.5)',]} style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, borderRadius: 15, justifyContent: 'center' }}>
-                                                <Text style={[styles.textDefault, { color: '#fff' }]}>Next</Text>
-                                            </LinearGradient>
-                                        </TouchableOpacity>
-                                        <TouchableOpacity onPress={() => props.navigation.goBack()} style={[styles.box, { backgroundColor: '#5A647F' }]} >
-                                            <Text style={[styles.textDefault, { color: '#fff' }]}>Back</Text>
-                                        </TouchableOpacity>
-                                    </View>
-                                </View>
-
-                            )
-                        }}
-                    </Formik >
+                            <CustomTextInput
+                                imageUri={require('../assets/images/user.png')}
+                                value={declareStamp}
+                                handleChange={FormikProps.handleChange(`declareStamp`)}
+                                handleBlur={FormikProps.handleBlur(`declareStamp`)}
+                                touched={declareStampTouched}
+                                error={declareStampError}
+                                placeholder={'Stamp'}
+                                keyboardType={'default'}
+                            />
 
 
-                </KeyboardAvoidingView>
-            </View>
 
-        </View>
+                            <View style={{ flexDirection: 'row', margin: 5 }}>
+                                <TouchableOpacity disabled={!FormikProps.isValid || !isInternetReachable} onPress={() => FormikProps.handleSubmit()} style={styles.box}>
+                                    <LinearGradient colors={(FormikProps.isValid && isInternetReachable) ? ['#4DCB3E', '#269B1D',] : ['rgba(77,203,62,0.5)', 'rgba(38,155,29,0.5)',]} style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, borderRadius: 15, justifyContent: 'center' }}>
+                                        <Text style={[styles.textDefault, { color: '#fff' }]}>Next</Text>
+                                    </LinearGradient>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => props.navigation.goBack()} style={[styles.box, { backgroundColor: '#5A647F' }]} >
+                                    <Text style={[styles.textDefault, { color: '#fff' }]}>Back</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
+
+                    )
+                }}
+            </Formik >
+
+
+        </LayoutA>
     );
 }
 
