734a9e1a1a35d6f057bb8563572a82a95d138abc
Hafizh <eva_01@yahoo.com>
2020-03-19 13:46:39 +0800

    Implement CustomFormAction at CompanyContactInformation

--- src/screens/CompanyContactInformationScreen.js
+++ src/screens/CompanyContactInformationScreen.js
@@ -21,7 +21,7 @@ import styles from '../styles/styles'
 import { Formik } from 'formik';
 import * as Yup from 'yup';
 import * as actionCreator from '../store/actions/action'
-import { CustomTextInput } from '../components/Custom'
+import { CustomTextInput,CustomFormAction } from '../components/Custom'
 
 const validationSchema = Yup.object().shape({
 
@@ -150,16 +150,11 @@ const CompanyContactInformationScreen = (props) => {
                                 {comp_addressTouched && comp_addressError && <Text style={styles.error}>{comp_addressError}</Text>}
                             </View>
 
-                            <View style={{ flexDirection: 'row', margin: 5 }}>
-                                <TouchableOpacity disabled={!FormikProps.isValid || !isInternetReachable} onPress={() => FormikProps.handleSubmit()} style={styles.box}>
-                                    <LinearGradient colors={(FormikProps.isValid && isInternetReachable) ? ['#4DCB3E', '#269B1D',] : ['rgba(77,203,62,0.5)', 'rgba(38,155,29,0.5)',]} style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, borderRadius: 15, justifyContent: 'center' }}>
-                                        <Text style={[styles.textDefault, { color: '#fff' }]}>Next</Text>
-                                    </LinearGradient>
-                                </TouchableOpacity>
-                                <TouchableOpacity onPress={() => props.navigation.goBack()} style={[styles.box, { backgroundColor: '#5A647F' }]} >
-                                    <Text style={[styles.textDefault, { color: '#fff' }]}>Back</Text>
-                                </TouchableOpacity>
-                            </View>
+                            <CustomFormAction
+                                navigation={props.navigation}
+                                isValid={FormikProps.isValid}
+                                handleSubmit={FormikProps.handleSubmit}
+                            />
                         </View>
 
                     )
