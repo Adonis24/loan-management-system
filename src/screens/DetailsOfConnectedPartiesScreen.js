5b41246229bb9cd03bfc41c6b80f78d880ea4876
Hafizh <eva_01@yahoo.com>
2020-03-19 13:48:06 +0800

    Implement CustomFormAction at DetailsOfConnectedParties

--- src/screens/DetailsOfConnectedPartiesScreen.js
+++ src/screens/DetailsOfConnectedPartiesScreen.js
@@ -1,5 +1,5 @@
 //console.ignoredYellowBox = ['Setting a timer']
-import React,{ useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     Image,
     Text,
@@ -15,7 +15,7 @@ import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 import Constants from 'expo-constants'
 import { LinearGradient } from 'expo-linear-gradient'
 import Layout from '../constants/Layout'
-import { CustomTextInput } from '../components/Custom'
+import { CustomTextInput,CustomFormAction } from '../components/Custom'
 import { keyboardBeingDisplay, keyboardBeingClose } from '../components/handleKeyboard'
 
 import styles from '../styles/styles'
@@ -92,123 +92,118 @@ const DetailsOfConnectedPartiesScreen = (props) => {
         <LayoutA>
 
 
-                    <Formik
-                        validateOnMount
-                        initialValues={{}}
+            <Formik
+                validateOnMount
+                initialValues={{}}
 
-                        onSubmit={async (values, actions) => {
-                            console.log(`values formik ialah ${JSON.stringify(values)}`)
-                            await setDetailConnect(values)
-                            DetailConnect()
-                            //props.navigation.navigate('CompanyInfoSuccess')
-                            actions.setSubmitting(false)
-                        }
-                        }
-                        validationSchema={validationSchema}
-                    >
-                        {FormikProps => {
-
-
+                onSubmit={async (values, actions) => {
+                    console.log(`values formik ialah ${JSON.stringify(values)}`)
+                    await setDetailConnect(values)
+                    DetailConnect()
+                    //props.navigation.navigate('CompanyInfoSuccess')
+                    actions.setSubmitting(false)
+                }
+                }
+                validationSchema={validationSchema}
+            >
+                {FormikProps => {
 
 
-                            const { capacity, nameCP, icNumber, relationship, emailSME } = FormikProps.values
-
-                            const capacityError = FormikProps.errors.capacity
-                            const capacityTouched = FormikProps.touched.capacity
-
-                            const nameCPError = FormikProps.errors.nameCP
-                            const nameCPTouched = FormikProps.touched.nameCP
-
-                            const icNumberError = FormikProps.errors.icNumber
-                            const icNumberTouched = FormikProps.touched.icNumber
-
-                            const relationshipError = FormikProps.errors.relationship
-                            const relationshipTouched = FormikProps.touched.relationship
-
-                            const emailSMEError = FormikProps.errors.emailSME
-                            const emailSMETouched = FormikProps.touched.emailSME
 
 
-                            return (
+                    const { capacity, nameCP, icNumber, relationship, emailSME } = FormikProps.values
 
-                                <View style={{ width: Layout.window.width * 0.8, justifyContent: 'center', alignItems: 'center' }}>
-                                  {showLogo&& <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.7 }} resizeMode={'contain'} /> }
-                                    <Text style={[styles.textDefault, { margin: 5, fontWeight: 'bold' }]}>REGISTRATION</Text>
-                                    {/* <Image source={require('../assets/images/1.png')} style={{ height: 50, width: 200, margin: 5 }} resizeMode={'stretch'} /> */}
-                                    <Text style={[styles.textDefault, { margin: 5, color: 'darkblue' }]}>Details of Connected Party</Text>
+                    const capacityError = FormikProps.errors.capacity
+                    const capacityTouched = FormikProps.touched.capacity
 
+                    const nameCPError = FormikProps.errors.nameCP
+                    const nameCPTouched = FormikProps.touched.nameCP
 
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/user.png')}
-                                        value={capacity}
-                                        handleChange={FormikProps.handleChange(`capacity`)}
-                                        handleBlur={FormikProps.handleBlur(`capacity`)}
-                                        touched={capacityTouched}
-                                        error={capacityError}
-                                        placeholder={'Capacity'}
+                    const icNumberError = FormikProps.errors.icNumber
+                    const icNumberTouched = FormikProps.touched.icNumber
 
-                                    />
+                    const relationshipError = FormikProps.errors.relationship
+                    const relationshipTouched = FormikProps.touched.relationship
 
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/mykad.png')}
-                                        value={nameCP}
-                                        handleChange={FormikProps.handleChange(`nameCP`)}
-                                        handleBlur={FormikProps.handleBlur(`nameCP`)}
-                                        touched={nameCPTouched}
-                                        error={nameCPError}
-                                        placeholder={'Name of Connected Party'}
-                                        keyboardType={'default'}
-                                    />
-
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/mykad.png')}
-                                        value={icNumber}
-                                        handleChange={FormikProps.handleChange(`icNumber`)}
-                                        handleBlur={FormikProps.handleBlur(`icNumber`)}
-                                        touched={icNumberTouched}
-                                        error={icNumberError}
-                                        placeholder={'MyKad No'}
-                                        keyboardType={'numbers-and-punctuation'}
-                                    />
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/user.png')}
-                                        value={relationship}
-                                        handleChange={FormikProps.handleChange(`relationship`)}
-                                        handleBlur={FormikProps.handleBlur(`relationship`)}
-                                        touched={relationshipTouched}
-                                        error={relationshipError}
-                                        placeholder={'Relationship'}
-                                        keyboardType={'default'}
-                                    />
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/email.png')}
-                                        value={emailSME}
-                                        handleChange={FormikProps.handleChange(`emailSME`)}
-                                        handleBlur={FormikProps.handleBlur(`emailSME`)}
-                                        touched={emailSMETouched}
-                                        error={emailSMEError}
-                                        placeholder={'Email SME Bank'}
-                                        keyboardType={'email-address'}
-                                    />
+                    const emailSMEError = FormikProps.errors.emailSME
+                    const emailSMETouched = FormikProps.touched.emailSME
 
 
-                                    <View style={{ flexDirection: 'row', margin: 5 }}>
-                                        <TouchableOpacity disabled={!FormikProps.isValid || !isInternetReachable} onPress={() => FormikProps.handleSubmit()} style={styles.box}>
-                                            <LinearGradient colors={(FormikProps.isValid && isInternetReachable) ? ['#4DCB3E', '#269B1D',] : ['rgba(77,203,62,0.5)', 'rgba(38,155,29,0.5)',]} style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, borderRadius: 15, justifyContent: 'center' }}>
-                                                <Text style={[styles.textDefault, { color: '#fff' }]}>Next</Text>
-                                            </LinearGradient>
-                                        </TouchableOpacity>
-                                        <TouchableOpacity onPress={() => props.navigation.goBack()} style={[styles.box, { backgroundColor: '#5A647F' }]} >
-                                            <Text style={[styles.textDefault, { color: '#fff' }]}>Back</Text>
-                                        </TouchableOpacity>
-                                    </View>
-                                </View>
+                    return (
 
-                            )
-                        }}
-                    </Formik >
+                        <View style={{ width: Layout.window.width * 0.8, justifyContent: 'center', alignItems: 'center' }}>
+                            {showLogo && <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.7 }} resizeMode={'contain'} />}
+                            <Text style={[styles.textDefault, { margin: 5, fontWeight: 'bold' }]}>REGISTRATION</Text>
+                            {/* <Image source={require('../assets/images/1.png')} style={{ height: 50, width: 200, margin: 5 }} resizeMode={'stretch'} /> */}
+                            <Text style={[styles.textDefault, { margin: 5, color: 'darkblue' }]}>Details of Connected Party</Text>
 
-</LayoutA>
+
+                            <CustomTextInput
+                                imageUri={require('../assets/images/user.png')}
+                                value={capacity}
+                                handleChange={FormikProps.handleChange(`capacity`)}
+                                handleBlur={FormikProps.handleBlur(`capacity`)}
+                                touched={capacityTouched}
+                                error={capacityError}
+                                placeholder={'Capacity'}
+
+                            />
+
+                            <CustomTextInput
+                                imageUri={require('../assets/images/mykad.png')}
+                                value={nameCP}
+                                handleChange={FormikProps.handleChange(`nameCP`)}
+                                handleBlur={FormikProps.handleBlur(`nameCP`)}
+                                touched={nameCPTouched}
+                                error={nameCPError}
+                                placeholder={'Name of Connected Party'}
+                                keyboardType={'default'}
+                            />
+
+                            <CustomTextInput
+                                imageUri={require('../assets/images/mykad.png')}
+                                value={icNumber}
+                                handleChange={FormikProps.handleChange(`icNumber`)}
+                                handleBlur={FormikProps.handleBlur(`icNumber`)}
+                                touched={icNumberTouched}
+                                error={icNumberError}
+                                placeholder={'MyKad No'}
+                                keyboardType={'numbers-and-punctuation'}
+                            />
+                            <CustomTextInput
+                                imageUri={require('../assets/images/user.png')}
+                                value={relationship}
+                                handleChange={FormikProps.handleChange(`relationship`)}
+                                handleBlur={FormikProps.handleBlur(`relationship`)}
+                                touched={relationshipTouched}
+                                error={relationshipError}
+                                placeholder={'Relationship'}
+                                keyboardType={'default'}
+                            />
+                            <CustomTextInput
+                                imageUri={require('../assets/images/email.png')}
+                                value={emailSME}
+                                handleChange={FormikProps.handleChange(`emailSME`)}
+                                handleBlur={FormikProps.handleBlur(`emailSME`)}
+                                touched={emailSMETouched}
+                                error={emailSMEError}
+                                placeholder={'Email SME Bank'}
+                                keyboardType={'email-address'}
+                            />
+
+
+                            <CustomFormAction
+                                navigation={props.navigation}
+                                isValid={FormikProps.isValid}
+                                handleSubmit={FormikProps.handleSubmit}
+                            />
+                        </View>
+
+                    )
+                }}
+            </Formik >
+
+        </LayoutA>
     );
 }
 
