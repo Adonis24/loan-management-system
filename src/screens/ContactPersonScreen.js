86e75e78cf5bae4b8e6ede040d2be9f131a4d897
Hafizh <eva_01@yahoo.com>
2020-03-19 13:16:32 +0800

    add layout A and immplement show logo at ContactPerson

--- src/screens/ContactPersonScreen.js
+++ src/screens/ContactPersonScreen.js
@@ -1,5 +1,5 @@
 //console.ignoredYellowBox = ['Setting a timer']
-import React from 'react';
+import React,{ useEffect, useState } from 'react';
 import {
     Image,
     Text,
@@ -11,6 +11,7 @@ import {
 } from 'react-native';
 import { shallowEqual, useSelector, useDispatch } from 'react-redux'
 import Constants from 'expo-constants'
+import { keyboardBeingDisplay, keyboardBeingClose } from '../components/handleKeyboard'
 
 import { LinearGradient } from 'expo-linear-gradient'
 import * as DocumentPicker from 'expo-document-picker';
@@ -20,6 +21,7 @@ import * as Yup from 'yup';
 import styles from '../styles/styles'
 import { CustomTextInput } from '../components/Custom'
 import * as actionCreator from '../store/actions/action'
+import LayoutA from '../Layout/LayoutA';
 
 const validationSchema = Yup.object().shape({
 
@@ -76,127 +78,127 @@ const ContactPersonScreen = (props) => {
         props.navigation.navigate('CompanyInfoSuccess')
     }
 
+    useEffect(() => {
+        const open = () => setshowLogo(false)
+        const off = () => setshowLogo(true)
+
+        keyboardBeingDisplay(open)
+        keyboardBeingClose(off)
+    }, []); // empty-array means don't watch for any updates
+
+    const [showLogo, setshowLogo] = useState(true)
+
 
     return (
-        <View style={styles.container}>
-            <View style={{ flex: 1, justifyContent: 'flex-start' }}>
-                <Image source={require('../assets/images/tekunA.png')} style={{ width: Layout.window.width, }} resizeMode={'contain'} />
-            </View>
-            <View style={{ position: 'absolute', top: 0, left: 0, bottom: 0, right: 0, }}>
-                <KeyboardAvoidingView behavior="padding" enabled style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <LayoutA>
+            <Formik
+                validateOnMount
+                initialValues={{ full_name: undefined, ic_no: undefined, phone: undefined, position: undefined }}
 
-                    <Formik
-                        validateOnMount
-                        initialValues={{ full_name: undefined, ic_no: undefined, phone: undefined, position: undefined }}
+                onSubmit={(values, actions) => {
+                    console.log(`values formik ialah ${JSON.stringify(values)}`)
+                    setContactPerson(values)
+                    ContactPerson(values)
+                    //props.navigation.navigate('CompanyInfoSuccess')
+                    actions.setSubmitting(false)
+                }
+                }
+                validationSchema={validationSchema}
+            >
+                {FormikProps => {
 
-                        onSubmit={(values, actions) => {
-                            console.log(`values formik ialah ${JSON.stringify(values)}`)
-                            setContactPerson(values)
-                            ContactPerson(values)
-                            //props.navigation.navigate('CompanyInfoSuccess')
-                            actions.setSubmitting(false)
-                        }
-                        }
-                        validationSchema={validationSchema}
-                    >
-                        {FormikProps => {
+                    const { full_name, position, ic_no, phone } = FormikProps.values
 
-                            const { full_name, position, ic_no, phone } = FormikProps.values
+                    const full_nameError = FormikProps.errors.full_name
+                    const full_nameTouched = FormikProps.touched.full_name
 
-                            const full_nameError = FormikProps.errors.full_name
-                            const full_nameTouched = FormikProps.touched.full_name
+                    const positionError = FormikProps.errors.position
+                    const positionTouched = FormikProps.touched.position
 
-                            const positionError = FormikProps.errors.position
-                            const positionTouched = FormikProps.touched.position
+                    const ic_noError = FormikProps.errors.ic_no
+                    const ic_noTouched = FormikProps.touched.ic_no
 
-                            const ic_noError = FormikProps.errors.ic_no
-                            const ic_noTouched = FormikProps.touched.ic_no
-
-                            const phoneError = FormikProps.errors.phone
-                            const phoneTouched = FormikProps.touched.phone
+                    const phoneError = FormikProps.errors.phone
+                    const phoneTouched = FormikProps.touched.phone
 
 
-                            return (
+                    return (
 
-                                <View style={{ width: Layout.window.width * 0.8, justifyContent: 'center', alignItems: 'center' }}>
-                                    <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.7 }} resizeMode={'contain'} />
-                                    <Text style={[styles.textDefault, { margin: 5, fontWeight: 'bold' }]}>CONTACT PERSON</Text>
-                                    <Text style={[styles.textDefault, { margin: 5, marginBottom: 10, color: 'darkblue', fontSize: 14 }]}>Please fill up this form to continue the process for contact person.</Text>
+                        <View style={{ width: Layout.window.width * 0.8, justifyContent: 'center', alignItems: 'center' }}>
+                           {showLogo && <Image source={require('../assets/images/logo.png')} style={{ height: Layout.window.height * 0.2, width: Layout.window.width * 0.7 }} resizeMode={'contain'} /> }
+                            <Text style={[styles.textDefault, { margin: 5, fontWeight: 'bold' }]}>CONTACT PERSON</Text>
+                            <Text style={[styles.textDefault, { margin: 5, marginBottom: 10, color: 'darkblue', fontSize: 14 }]}>Please fill up this form to continue the process for contact person.</Text>
 
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/user.png')}
-                                        value={full_name}
-                                        handleChange={FormikProps.handleChange(`full_name`)}
-                                        handleBlur={FormikProps.handleBlur(`full_name`)}
-                                        touched={full_nameTouched}
-                                        error={full_nameError}
-                                        placeholder={'Full Name'}
+                            <CustomTextInput
+                                imageUri={require('../assets/images/user.png')}
+                                value={full_name}
+                                handleChange={FormikProps.handleChange(`full_name`)}
+                                handleBlur={FormikProps.handleBlur(`full_name`)}
+                                touched={full_nameTouched}
+                                error={full_nameError}
+                                placeholder={'Full Name'}
 
-                                    />
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/mykad.png')}
-                                        value={ic_no}
-                                        handleChange={FormikProps.handleChange(`ic_no`)}
-                                        handleBlur={FormikProps.handleBlur(`ic_no`)}
-                                        touched={ic_noTouched}
-                                        error={ic_noError}
-                                        placeholder={'MyKad No'}
-                                        keyboardType={'decimal-pad'}
-                                    />
+                            />
+                            <CustomTextInput
+                                imageUri={require('../assets/images/mykad.png')}
+                                value={ic_no}
+                                handleChange={FormikProps.handleChange(`ic_no`)}
+                                handleBlur={FormikProps.handleBlur(`ic_no`)}
+                                touched={ic_noTouched}
+                                error={ic_noError}
+                                placeholder={'MyKad No'}
+                                keyboardType={'decimal-pad'}
+                            />
 
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/position.png')}
-                                        value={position}
-                                        handleChange={FormikProps.handleChange(`position`)}
-                                        handleBlur={FormikProps.handleBlur(`position`)}
-                                        touched={positionTouched}
-                                        error={positionError}
-                                        placeholder={'Position'}
-                                        keyboardType={'default'}
-                                    />
-                                    <CustomTextInput
-                                        imageUri={require('../assets/images/phoneNum.png')}
-                                        value={phone}
-                                        handleChange={FormikProps.handleChange(`phone`)}
-                                        handleBlur={FormikProps.handleBlur(`phone`)}
-                                        touched={phoneTouched}
-                                        error={phoneError}
-                                        placeholder={'Phone'}
-                                        keyboardType={'decimal-pad'}
-                                    />
+                            <CustomTextInput
+                                imageUri={require('../assets/images/position.png')}
+                                value={position}
+                                handleChange={FormikProps.handleChange(`position`)}
+                                handleBlur={FormikProps.handleBlur(`position`)}
+                                touched={positionTouched}
+                                error={positionError}
+                                placeholder={'Position'}
+                                keyboardType={'default'}
+                            />
+                            <CustomTextInput
+                                imageUri={require('../assets/images/phoneNum.png')}
+                                value={phone}
+                                handleChange={FormikProps.handleChange(`phone`)}
+                                handleBlur={FormikProps.handleBlur(`phone`)}
+                                touched={phoneTouched}
+                                error={phoneError}
+                                placeholder={'Phone'}
+                                keyboardType={'decimal-pad'}
+                            />
 
 
-                                    <View style={{ alignSelf: 'stretch', borderWidth: 1, borderRadius: 15, borderColor: 'darkblue', margin: 10, justifyContent: 'space-between', alignItems: 'flex-end', flexDirection: 'row' }}>
-                                        {!name ? <Text style={[styles.caption, { alignSelf: 'flex-start', textAlign: 'left', margin: 10, padding: 10 }]}>MyKad Scanned Copy</Text> :
-                                            <Text style={[styles.textDefault, { alignSelf: 'flex-start', textAlign: 'left', margin: 10, padding: 10 }]}>{name}</Text>}
-                                        <TouchableOpacity onPress={() => pickDoc()} style={{ padding: 10, borderRadius: 5, justifyContent: 'center', backgroundColor: 'gainsboro', margin: 10 }}>
-                                            <Text style={[styles.caption, { color: '#000', fontSize: 10 }]}>Select</Text>
-                                        </TouchableOpacity>
-                                    </View>
+                            <View style={{ alignSelf: 'stretch', borderWidth: 1, borderRadius: 15, borderColor: 'darkblue', margin: 10, justifyContent: 'space-between', alignItems: 'flex-end', flexDirection: 'row' }}>
+                                {!name ? <Text style={[styles.caption, { alignSelf: 'flex-start', textAlign: 'left', margin: 10, padding: 10 }]}>MyKad Scanned Copy</Text> :
+                                    <Text style={[styles.textDefault, { alignSelf: 'flex-start', textAlign: 'left', margin: 10, padding: 10 }]}>{name}</Text>}
+                                <TouchableOpacity onPress={() => pickDoc()} style={{ padding: 10, borderRadius: 5, justifyContent: 'center', backgroundColor: 'gainsboro', margin: 10 }}>
+                                    <Text style={[styles.caption, { color: '#000', fontSize: 10 }]}>Select</Text>
+                                </TouchableOpacity>
+                            </View>
 
 
 
 
-                                    <View style={{ flexDirection: 'row', margin: 5 }}>
-                                        <TouchableOpacity disabled={!FormikProps.isValid || !isInternetReachable} onPress={() => FormikProps.handleSubmit()} style={styles.box}>
-                                            <LinearGradient colors={(FormikProps.isValid && isInternetReachable) ? ['#4DCB3E', '#269B1D',] : ['rgba(77,203,62,0.5)', 'rgba(38,155,29,0.5)',]} style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, borderRadius: 15, justifyContent: 'center' }}>
-                                                <Text style={[styles.textDefault, { color: '#fff' }]}>Next</Text>
-                                            </LinearGradient>
-                                        </TouchableOpacity>
-                                        <TouchableOpacity onPress={() => props.navigation.goBack()} style={[styles.box, { backgroundColor: '#5A647F' }]} >
-                                            <Text style={[styles.textDefault, { color: '#fff' }]}>Back</Text>
-                                        </TouchableOpacity>
-                                    </View>
-                                </View>
+                            <View style={{ flexDirection: 'row', margin: 5 }}>
+                                <TouchableOpacity disabled={!FormikProps.isValid || !isInternetReachable} onPress={() => FormikProps.handleSubmit()} style={styles.box}>
+                                    <LinearGradient colors={(FormikProps.isValid && isInternetReachable) ? ['#4DCB3E', '#269B1D',] : ['rgba(77,203,62,0.5)', 'rgba(38,155,29,0.5)',]} style={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, borderRadius: 15, justifyContent: 'center' }}>
+                                        <Text style={[styles.textDefault, { color: '#fff' }]}>Next</Text>
+                                    </LinearGradient>
+                                </TouchableOpacity>
+                                <TouchableOpacity onPress={() => props.navigation.goBack()} style={[styles.box, { backgroundColor: '#5A647F' }]} >
+                                    <Text style={[styles.textDefault, { color: '#fff' }]}>Back</Text>
+                                </TouchableOpacity>
+                            </View>
+                        </View>
 
-                            )
-                        }}
-                    </Formik >
-
-
-                </KeyboardAvoidingView>
-            </View>
-        </View>
+                    )
+                }}
+            </Formik >
+        </LayoutA>
     );
 }
 
