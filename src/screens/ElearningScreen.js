76c91531892248edee7e40e4916d4829207ff96f
Syahrizan Ali Hassan <syahrizan@fintechcapital.co>
2020-03-20 16:27:37 +0800

    Elearning wip

--- src/screens/ElearningScreen.js
+++ src/screens/ElearningScreen.js
@@ -3,6 +3,7 @@ import React from 'react';
 import {
     TouchableOpacity,
     View,
+    Image
 
 } from 'react-native';
 import { WebView } from 'react-native-webview';
@@ -10,31 +11,40 @@ import styles from '../styles/styles'
 
 import Constants from 'expo-constants'
 import { shallowEqual, useSelector } from 'react-redux'
-import Layout from '../constants/Layout'
+import LayoutB from '../Layout/LayoutB'
 
 
 const ElearningScreen = (props) => {
-
-    const nav = (screen) => {
-        this.props.navigation.navigate(screen)
-    }
 
     const { jwt } = useSelector(state => state.myAccountReducer, shallowEqual)
     console.log(`jwt ialah ${jwt}`)
     const uri = `https://lms.bxcess.my/courses/?bx-token=${jwt}`
 
-    return (
-        <View style={styles.container}>
-            <WebView source={{ uri }} style={{ flex: 1, backgroundColor: 'transparent' }} />
-            <View style={{ position: 'absolute', top: Constants.statusBarHeight, left: 0, right: 0, flex: 1, flexDirection: 'row', justifyContent: 'center', alignItems: 'center' }}>
-                <View style={{ flex: 1, alignSelf: 'stretch', justifyContent: 'center', border: 1, borderColor: '#000', alignItems: 'center' }}>
-                    <TouchableOpacity onPress={() => props.navigation.goBack()} hitSlop={{ top: 5, left: 5, bottom: 5, right: 5 }}>
-                        <View style={{ borderWidth: 3, borderColor: '#fff', width: Layout.window.width / 5 }} />
-                    </TouchableOpacity>
-                </View>
 
-            </View>
-        </View>
+    const modifiedStyle = `document.querySelector("#breadcrumb").setAttribute("style", "display:none");
+    document.querySelector("#slide").setAttribute("style", "display:none");
+    
+    
+   ;`
+    return (
+
+
+        < LayoutB
+            title={'E-Learning'}
+            screenType='form'
+            navigation={props.navigation}
+            imageUri={require('../assets/images/news.png')}
+        >
+            <WebView
+                source={{ uri }}
+                style={{ flex: 1, backgroundColor: 'transparent' }}
+                javaScriptEnabled={true}
+                domStorageEnabled={true}
+                injectedJavaScript={modifiedStyle} />
+
+        </ LayoutB>
+
+
 
     );
 
