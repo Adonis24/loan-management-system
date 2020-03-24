6447319e5194f66db848b4fbef9c414d9bc3495f
Syahrizan Ali Hassan <syahrizan@fintechcapital.co>
2020-03-24 17:54:38 +0800

    .

--- src/screens/FinancingScreen.js
+++ src/screens/FinancingScreen.js
@@ -76,7 +76,7 @@ const FinancingScreen = (props) => {
                     <View style={{ flex: 4, marginTop: 5, marginBottom: 5, paddingTop: 5, paddingBottom: 5 }}>
                         <View style={[{ backgroundColor: '#fff', marginLeft: Layout.window.width / 8, borderBottomLeftRadius: 20, borderTopLeftRadius: 20, borderWidth: 1, borderRightWidth: 0, borderColor: 'lightgrey', flexDirection: 'row', elevation: 2, justifyContent: 'flex-start' }]}>
                             <Image source={require('../assets/images/e-scoring.png')} style={{ width: Layout.window.height / 15, height: Layout.window.height / 15, margin: 5 }} resizeMode={'contain'} />
-                            <Text style={[styles.default, { alignSelf: 'center', fontSize: 18, fontWeight: "bold" }]} numberOfLines={1} ellipsizeMode={'tail'}>Financing & Grant</Text>
+                            <Text style={[styles.default, { alignSelf: 'center', fontSize: 18, fontWeight: "bold" }]} numberOfLines={1} ellipsizeMode={'tail'}>Financing</Text>
                         </View>
                     </View>
                 </View>
