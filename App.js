3d0203fbd53fbaef8f14ecb09d057a35f7f8e505
Syahrizan Ali Hassan <syahrizan@fintechcapital.co>
2020-05-14 12:48:41 +0800

    adding splash api for splash screen manipulation

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, View, Text, StatusBar } from 'react-native';
 import { Asset } from 'expo-asset'
 import * as Font from 'expo-font'
 import * as Permissions from 'expo-permissions'
-import { AppLoading } from 'expo';
-import { Notifications } from 'expo';
+import { AppLoading, Notifications } from 'expo';
+import * as SplashScreen from 'expo-splash-screen';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
@@ -22,17 +22,13 @@ import Nav from './src/navigation/Nav';
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
 const App = (props) => {
-
   const [isLoadingComplete, setIsLoadingComplete] = useState(false)
-
   const [isInternetReachable, setNetInfo] = useState(null)
-
   const registerForPushNotificationsAsync = async () => {
     const { status: existingStatus } = await Permissions.getAsync(
       Permissions.NOTIFICATIONS
     );
     let finalStatus = existingStatus;
-
     if (existingStatus !== 'granted') {
       const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
       finalStatus = status;
@@ -45,25 +41,17 @@ const App = (props) => {
 
     // Get the token that uniquely identifies this device
     let token = await Notifications.getExpoPushTokenAsync();
-     console.log(`expo token ialah ${token}`)
+    console.log(`expo token ialah ${token}`)
     store.dispatch({ type: 'SET_REGISTER', payload: { expo_token: token } })
-  
   }
 
-
   useEffect(() => {
-    checkUpdate()
-
-    registerForPushNotificationsAsync();
-    const _notificationSubscription = Notifications.addListener(_handleNotification);
-    const netInfoUnsubscribe = NetInfo.addEventListener(_handleNetInfo);
-    //checkLogin()
+    loadResourcesAndDataAsync()
 
   }, [])
 
   const _handleNotification = (notification) => {
     const { data } = notification
-
     store.dispatch({ type: 'SET_NOTIFICATION', payload: { ...data } })
   };
 
@@ -79,15 +67,27 @@ const App = (props) => {
     }
   }
 
+  const loadResourcesAndDataAsync = async () => {
+    try {
+      SplashScreen.preventAutoHideAsync();
+      checkUpdate();
+      registerForPushNotificationsAsync();
+      const _notificationSubscription = Notifications.addListener(_handleNotification);
+      const netInfoUnsubscribe = NetInfo.addEventListener(_handleNetInfo);
+
+    }
+    catch (e) {
+      console.warn(e)
+    }
+    finally {
+      setIsLoadingComplete(true)
+      SplashScreen.hideAsync()
+    }
+  }
 
 
-  const _handleLoadingError = error => {
-    console.warn(error);
-  };
-
-  const _handleFinishLoading = () => {
-    setIsLoadingComplete(true)
-  };
+  const _handleLoadingError = error => { console.warn(error); };
+  const _handleFinishLoading = () => { setIsLoadingComplete(true) };
 
   const _handleNetInfo = (netInfo) => {
     store.dispatch({ type: 'SET_NET_INFO_STATUS', payload: { ...netInfo } })
@@ -124,9 +124,7 @@ const _loadResourcesAsync = async () => {
     Asset.loadAsync([
       require('./src/assets/images/robot-dev.png'),
       require('./src/assets/images/robot-prod.png'),
-
       require('./src/assets/images/logo.png'),
-
       require('./src/assets/images/topLeft.png'),
       require('./src/assets/images/bottomRight.png'),
       require('./src/assets/images/topRight.png'),
@@ -136,45 +134,34 @@ const _loadResourcesAsync = async () => {
       require('./src/assets/images/tekunC.png'),
       require('./src/assets/images/tekunD.png'),
       require('./src/assets/images/tekunE.png'),
-
       require('./src/assets/images/girl.png'),
       require('./src/assets/images/intro2.png'),
       require('./src/assets/images/loan.png'),
       require('./src/assets/images/cardborder.png'),
-
       require('./src/assets/icon/eda.png'),
       require('./src/assets/icon/bizDir.png'),
       require('./src/assets/icon/crm.png'),
-      //require('./src/assets/icon/crm.png'),
       require('./src/assets/icon/rfq.png'),
-
       require('./src/assets/images/edt.png'),
       require('./src/assets/images/edt2.jpg'),
-
       require('./src/assets/images/mayamall.png'),
-
       require('./src/assets/images/urusniaga.png'),
-
       require('./src/assets/images/faceborder.png'),
-
       require('./src/assets/icon/wallet.png'),
       require('./src/assets/icon/news.png'),
       require('./src/assets/icon/ecommerce.png'),
       require('./src/assets/icon/e-learning.png'),
       require('./src/assets/icon/delivery-truck.png'),
       require('./src/assets/icon/bill.png'),
-
       require('./src/assets/images/banner1.png'),
       require('./src/assets/images/banner2.png'),
       require('./src/assets/images/logo-white.png'),
-
     ]),
     Font.loadAsync({
       'Montserrat_medium': require('./src/assets/fonts/Montserrat/Montserrat-Medium.ttf'),
       'Montserrat_light': require('./src/assets/fonts/Montserrat/Montserrat-Light.ttf'),
       'Montserrat_bold': require('./src/assets/fonts/Montserrat/Montserrat-Bold.ttf'),
       'Roboto-regular': require('./src/assets/fonts/Roboto/Roboto-Regular.ttf'),
-
     }),
   ]);
 };
